127dba81b93f27d25235b825b7d5e9a8c8e0267e
Giga Bokuchava <mister_giga@live.com>
2021-10-23 01:05:25 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React at Agruni
         </a>
       </header>
     </div>
